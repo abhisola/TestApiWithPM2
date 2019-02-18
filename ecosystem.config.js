bb2e77ec80f3418b4274d7e1d0d4334b178c10d8
azizahtas <azizahtas@gmail.com>
2019-02-18 11:09:38 +0530

    Fixed Memory Restart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     instances: "max", // was one
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
+    max_memory_restart: '500M',
     env: {
       NODE_ENV: 'development'
     },
